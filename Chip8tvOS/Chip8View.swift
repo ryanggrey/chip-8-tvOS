a43dbe969b9d290d90d36b1b46d13a3585091a26
Ryan Grey <ryan.g.grey@gmail.com>
2021-02-16 21:42:05 +0000

    Implement running emulator and loading space invaders

---
+++ Chip8tvOS/Chip8View.swift
@@ -0,0 +1,32 @@
+//
+//  Chip8View.swift
+//  CHIP-8
+//
+//  Created by Ryan Grey on 16/02/2021.
+//
+
+import UIKit
+import Chip8Emulator
+
+class Chip8View: UIView {
+    var screen: Chip8Screen?
+    var pixelColor: UIColor?
+
+    override func draw(_ layer: CALayer, in ctx: CGContext) {
+        super.draw(layer, in: ctx)
+        drawPixels(in: ctx)
+    }
+
+    private func drawPixels(in context: CGContext) {
+        guard let screen = screen, let pixelColor = pixelColor else { return }
+
+        let path = PathFactory.from(
+            screen: screen,
+            containerSize: self.frame.size,
+            isYReversed: false
+            )
+        context.setFillColor(pixelColor.cgColor)
+        context.addPath(path)
+        context.fillPath()
+    }
+}
--- Chip8tvOS/ViewController.swift
+++ Chip8tvOS/ViewController.swift
@@ -6,12 +6,78 @@
 //
 
 import UIKit
+import Chip8Emulator
 
 class ViewController: UIViewController {
+    private var chip8: Chip8!
+    private var loadedRom: [Byte]?
+    private var cpuTimer: Timer?
+    private var displayTimer: Timer?
+    private let cpuHz: TimeInterval = 1/600
+    private let displayHz: TimeInterval = 1/60
+    private let romName = "Space Invaders [David Winter]"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        guard let romData = NSDataAsset(name: romName)?.data else { return }
+        let rom = [Byte](romData)
+        let ram = RomLoader.loadRam(from: rom)
+
+        runEmulator(with: ram)
+    }
+
+    private func runEmulator(with rom: [Byte]) {
+        var chipState = ChipState()
+        chipState.ram = rom
+
+        self.chip8 = Chip8(
+            state: chipState,
+            cpuHz: cpuHz
+        )
+
+        startCpu()
+        startRendering()
+    }
+
+    private func startCpu() {
+        cpuTimer = Timer.scheduledTimer(
+            withTimeInterval: cpuHz,
+            repeats: true,
+            block: cpuTimerFired
+        )
+    }
+
+    private func cpuTimerFired(_: Timer) {
+        chip8.cycle()
+        if chip8.shouldPlaySound {
+            // TODO: play sound
+        }
+    }
+
+    private func startRendering() {
+        displayTimer = Timer.scheduledTimer(
+            withTimeInterval: displayHz,
+            repeats: true,
+            block: displayTimerFired
+        )
+    }
+
+    private func displayTimerFired(_: Timer) {
+        drawChip8IfNeeded()
+    }
+
+    private func drawChip8IfNeeded() {
+        if chip8.needsRedraw {
+            render(screen: chip8.screen)
+            chip8.needsRedraw = false
+        }
+    }
+
+    private func render(screen: Chip8Screen) {
+        guard let chip8View = self.view as? Chip8View else { return }
+        chip8View.pixelColor = .green
+        chip8View.screen = screen
+        chip8View.setNeedsDisplay()
     }
 }
-
