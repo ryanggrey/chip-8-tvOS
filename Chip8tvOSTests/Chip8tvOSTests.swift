cb7cb48f310efe158df3d438f7585a57905a39e2
Ryan Grey <ryan.g.grey@gmail.com>
2021-02-16 17:03:16 +0000

    Add stub project

---
+++ Chip8tvOSTests/Chip8tvOSTests.swift
@@ -0,0 +1,12 @@
+//
+//  Chip8tvOSTests.swift
+//  Chip8tvOSTests
+//
+//  Created by Ryan Grey on 16/02/2021.
+//
+
+import XCTest
+@testable import Chip8tvOS
+
+class Chip8tvOSTests: XCTestCase {
+}
---
+++ Chip8tvOSUITests/Chip8tvOSUITests.swift
@@ -0,0 +1,11 @@
+//
+//  Chip8tvOSUITests.swift
+//  Chip8tvOSUITests
+//
+//  Created by Ryan Grey on 16/02/2021.
+//
+
+import XCTest
+
+class Chip8tvOSUITests: XCTestCase {
+}
